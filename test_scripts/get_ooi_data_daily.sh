4a679bda3e39882d6e02e10cac9f12820552bd13
Kellen Rosburg <krosburg@eng.ooirsn.uw.edu>
2018-12-28 18:46:38 +0000

    formatting:

--- test_scripts/get_ooi_data_daily.sh
+++ test_scripts/get_ooi_data_daily.sh
@@ -4,7 +4,7 @@ FILES=$HOME/ooi-plotter/config/*.cfg
 #rm -f /var/www/html/status/inst_status_m2m.csv
 for f in $FILES
 do
-  date && $HOME/miniconda3/bin/python -W ignore $HOME/ooi-plotter/loadData.py $f day /var/www/html/kcrtest/images
+  date && $HOME/miniconda3/bin/python -W ignore $HOME/ooi-plotter/loadData.py $f day /var/www/html/kcrtest/images && echo " "
   sleep 1
 done
 mv -f /var/www/html/status/inst_status_m2m.csv.tmp /var/www/html/status/inst_status_m2m.csv
