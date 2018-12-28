c9e5856f6e1880e07f73c7c2fb7752fccf3d454d
Kellen Rosburg <krosburg@eng.ooirsn.uw.edu>
2018-12-28 22:27:12 +0000

    format

--- test_scripts/get_ooi_3Ddata_daily.sh
+++ test_scripts/get_ooi_3Ddata_daily.sh
@@ -2,8 +2,7 @@
 FILES=$HOME/ooi-plotter/config/*.3dcfg
 for f in $FILES
 do
-  echo $f
-  date && python $HOME/ooi-plotter/loadData_3D.py $f day /var/www/html/kcrtest/images
+  echo -n "$(echo $f | cut -d'/' -f6) - "
+  date && python -W ignore $HOME/ooi-plotter/loadData_3D.py $f day /var/www/html/kcrtest/images && echo " "
   sleep 1
 done
-
