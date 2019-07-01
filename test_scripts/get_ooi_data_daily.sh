944d029a39f432a9778f35d104ec300823568122
Kellen Rosburg <krosburg@eng.ooirsn.uw.edu>
2019-07-01 22:01:33 +0000

    Removed opOff status for 2019 Deployment

--- test_scripts/get_ooi_data_daily.sh
+++ test_scripts/get_ooi_data_daily.sh
@@ -4,8 +4,9 @@ FILES=$HOME/ooi-plotter/config/*.cfg
 #rm -f /var/www/html/status/inst_status_m2m.csv
 for f in $FILES
 do
-  echo -n "$(echo $f | cut -d'/' -f6) - "
-  date && $HOME/miniconda3/bin/python -W ignore $HOME/ooi-plotter/loadData.py $f day /var/www/html/kcrtest/images && echo " "
+    echo -n "$(echo $f | cut -d'/' -f6) - "
+    source activate py36
+    date && $HOME/miniconda3/bin/python -W ignore $HOME/ooi-plotter/loadData.py $f day /var/www/html/kcrtest/images && echo " "
   sleep 1
 done
 mv -f /var/www/html/status/inst_status_m2m.csv.tmp /var/www/html/status/inst_status_m2m.csv
