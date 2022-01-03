569cff4ad834353375e31d9d96ad1244f4110df8
Kellen Rosburg <krosburg@eng.ooirsn.uw.edu>
2022-01-03 19:41:43 +0000

    Updated run scripts

--- test_scripts/get_ooi_3Ddata_weekly.sh
+++ test_scripts/get_ooi_3Ddata_weekly.sh
@@ -2,7 +2,7 @@
 FILES=$HOME/ooi-plotter/config/*.3dcfg
 for f in $FILES
 do
-  date && python $HOME/ooi-plotter/loadData_3D.py $f week /var/www/html/kcrtest/images
+  date && python -W ignore $HOME/ooi-plotter/loadData_3D.py $f week /var/www/html/kcrtest/images
   sleep 1
 done
 
--- test_scripts/get_ooi_data_daily.sh
+++ test_scripts/get_ooi_data_daily.sh
@@ -5,7 +5,7 @@ FILES=$HOME/ooi-plotter/config/*.cfg
 for f in $FILES
 do
     echo -n "$(echo $f | cut -d'/' -f6) - "
-    source activate py36
+    conda activate py36
     date && $HOME/miniconda3/bin/python -W ignore $HOME/ooi-plotter/loadData.py $f day /var/www/html/kcrtest/images && echo " "
   sleep 1
 done
